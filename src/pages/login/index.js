44385e5f6a6fe71b0799addeb08dd8d18e667950
YeremiaIsrael <72957579+YeremiaIsrael@users.noreply.github.com>
2022-05-16 14:16:58 +0800

    add form styling

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,16 +1,17 @@
 import React,{useRef} from 'react'
 import {ForkSpoon,Cateringe} from '../../assets'
+import "./login.scss"
 
 const Login = (props) => {
   return (
-    <div>
-      <div>
-        <img src={Cateringe} alt='web logo'/>
+    <div className="login">
+      <div className="child">
+        <img src={Cateringe} alt='web logo' className="logo"/>
         <p>Its All About Food A Food</p>
         <p>We Here to Serve</p>
         <img src={ForkSpoon} alt='fork and spoon'/>
       </div>
-      <div>
+      <div className="child">
         <h1>Masuk</h1>
         <form>
           <div>
@@ -19,11 +20,11 @@ const Login = (props) => {
           </div>
           <div>
             <label>Nama Pengguna</label>
-            <input name="password" placeholder="**********"/>
+            <input name="password" placeholder="**********" type="password"/>
           </div>
-          <button>Masuk</button>
+          <button className="loginBtn">Masuk</button>
         </form>
-        <button>Daftar Akun</button>
+        <button className="registerBtn">Daftar Akun</button>
       </div>
     </div>
   )
